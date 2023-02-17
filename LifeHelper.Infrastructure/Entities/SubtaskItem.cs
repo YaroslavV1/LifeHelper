980919c56d59845b659c75fabdd79c1d99f21758
Yaroslav Stasenko <87314914+YaroslavV1@users.noreply.github.com>
2023-02-17 19:01:01 +0200

    Manage infrastructure #2
    feat - Created DbContext;
    feat - Created initial entities;
    feat - Created Configuration for entities;
    feat - Initialized connection string in the Program.cs.

---
+++ LifeHelper.Infrastructure/Entities/SubtaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace LifeHelper.Infrastructure.Entities;
+
+public class SubtaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    
+    public int TaskItemId { get; set; }
+    public TaskItem TaskItem { get; set; }
+}
---
+++ LifeHelper.Infrastructure/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace LifeHelper.Infrastructure.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Nickname { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    
+    public ICollection<TaskItem> TaskItems { get; set; }
+}
