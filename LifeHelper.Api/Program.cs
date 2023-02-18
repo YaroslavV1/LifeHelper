171da11a03d99e38d8b970a3e6bcb569744ee7dc
YaroslavV1 <Gaslotv@gmail.com>
2023-02-18 19:02:25 +0200

    feat - Initialized swagger description; feat - Configured swagger to use Xml comments.

--- LifeHelper.Api/Program.cs
+++ LifeHelper.Api/Program.cs
@@ -1,6 +1,8 @@
+using System.Reflection;
 using LifeHelper.Api.Middlewares;
 using LifeHelper.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "LifeHelper API",
+        Description = "An ASP.NET Core Web Api for my LifeHelper application"
+    });
+    
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 //Add DbContext
 builder.Services.AddDbContext<LifeHelperDbContext>(options =>
@@ -21,7 +34,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"));
 }
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
