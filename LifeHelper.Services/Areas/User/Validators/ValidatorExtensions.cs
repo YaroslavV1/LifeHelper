1e8eeee07a50417fd9e85c0fa0dfbd186aac5272
YaroslavV1 <Gaslotv@gmail.com>
2023-02-23 12:14:50 +0200

    fix - Fixed code review remarks.

--- LifeHelper.Services/Areas/User/Validators/ValidatorExtensions.cs
+++ LifeHelper.Services/Areas/User/Validators/ValidatorExtensions.cs
@@ -10,12 +10,12 @@ public static class ValidatorExtensions
             .NotEmpty()
                 .WithMessage("{PropertyName} is required!")
             .Length(8,32)
-                .WithMessage("{PropertyName} must be least 8 characters in length, but no more than 32")
+                .WithMessage("{PropertyName} must least 8 characters in length, but no more than 32")
             .Matches("[0-9]")
                 .WithMessage("{PropertyName} must least one digit")
             .Matches("[a-z]")
-                .WithMessage("{PropertyName} must least one lowercase character!")
+                .WithMessage("{PropertyName} must least one lowercase character")
             .Matches("[A-Z]")
-                .WithMessage("{PropertyName} least one uppercase character");
+                .WithMessage("{PropertyName} must least one uppercase character");
     }
 }
