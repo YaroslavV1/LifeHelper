9fef48677eb2b75ae3366a87c8e48ac4e3cb743a
YaroslavV1 <Gaslotv@gmail.com>
2023-02-23 10:29:21 +0200

    fix - Fixed code review remarks.

--- LifeHelper.Api/Program.cs
+++ LifeHelper.Api/Program.cs
@@ -6,11 +6,10 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
@@ -24,16 +23,14 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
-//Add DbContext
 builder.Services.AddDbContext<LifeHelperDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LifeHelperDatabase")));
 
-//Add AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
