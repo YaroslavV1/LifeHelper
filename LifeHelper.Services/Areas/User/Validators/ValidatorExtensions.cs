ff1d0f4e9f549f9bb4259c27669d6987812670e2
Yaroslav Stasenko <87314914+YaroslavV1@users.noreply.github.com>
2023-02-23 22:43:29 +0200

    Initialize FluentValidation #7
    *feat - Initialized FluentValidation;
    feat - Created validation for UserInput;
    feat - Injected FluentValidation to Program.cs.
    
    *fix - Fixed code review remarks.
    
    *fix - Fixed code review remarks.

---
+++ LifeHelper.Services/Areas/User/Validators/ValidatorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace LifeHelper.Services.Areas.User.Validators;
+
+public static class ValidatorExtensions
+{
+    public static IRuleBuilderOptions<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .Length(8,32).WithMessage("{PropertyName} must least from 8 to 32 characters")
+            .Matches("[0-9]").WithMessage("{PropertyName} must least one digit")
+            .Matches("[a-z]").WithMessage("{PropertyName} must least one lowercase character")
+            .Matches("[A-Z]").WithMessage("{PropertyName} must least one uppercase character");
+    }
+}
