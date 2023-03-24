451c39668e643c871d7765db1b24f9c7c43ef65d
YaroslavV1 <Gaslotv@gmail.com>
2023-03-24 16:06:30 +0200

    fix - Fixed UserService/Controller logic with admin/user control.

--- LifeHelper.Services/Areas/Helpers/Jwt/ClaimParserService.cs
+++ LifeHelper.Services/Areas/Helpers/Jwt/ClaimParserService.cs
@@ -4,13 +4,15 @@ using Microsoft.AspNetCore.Http;
 
 namespace LifeHelper.Services.Areas.Helpers.Jwt;
 
+using Infrastructure.Entities;
 public class ClaimParserService : IClaimParserService
 {
     public TokenInfoDto ParseInfoFromClaims(HttpContext context)
     {
         return new TokenInfoDto
         {
-            Id = Convert.ToInt32(context.User.FindFirstValue(ClaimTypes.NameIdentifier))
+            Id = Convert.ToInt32(context.User.FindFirstValue(ClaimTypes.NameIdentifier)),
+            Role = Convert.ToString(context.User.FindFirstValue(ClaimTypes.Role))
         };
     }
 }
--- LifeHelper.Services/Areas/Users/UserService.cs
+++ LifeHelper.Services/Areas/Users/UserService.cs
@@ -1,27 +1,35 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using LifeHelper.Infrastructure;
+using LifeHelper.Infrastructure.Entities;
 using LifeHelper.Infrastructure.Exceptions;
-using LifeHelper.Services.Areas.User.DTOs;
+using LifeHelper.Services.Areas.Helpers.Jwt;
+using LifeHelper.Services.Areas.Helpers.Jwt.DTOs;
+using LifeHelper.Services.Areas.Users.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using BadRequestException = LifeHelper.Infrastructure.Exceptions.BadRequestException;
 
-namespace LifeHelper.Services.Areas.User;
-
-using Infrastructure.Entities;
+namespace LifeHelper.Services.Areas.Users;
 
 public class UserService : IUserService
 {
     private readonly LifeHelperDbContext _dbContext;
     private readonly IMapper _mapper;
     private readonly PasswordHasher<User> _passwordHasher;
+    private readonly TokenInfoDto _currentUserInfo;
 
-    public UserService(LifeHelperDbContext dbContext, IMapper mapper)
+    public UserService(
+        LifeHelperDbContext dbContext,
+        IMapper mapper,
+        IHttpContextAccessor contextAccessor,
+        IClaimParserService claimParserService)
     {
         _dbContext = dbContext;
         _mapper = mapper;
         _passwordHasher = new PasswordHasher<User>();
+        _currentUserInfo = claimParserService.ParseInfoFromClaims(contextAccessor.HttpContext);
     }
     
     public async Task<IList<UserDto>> GetListAsync()
@@ -31,10 +39,14 @@ public class UserService : IUserService
 
     public async Task<UserDto> GetByIdAsync(int id)
     {
-        return await _dbContext.Users
-            .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
-            .FirstOrDefaultAsync(user => user.Id == id) 
-               ?? throw new NotFoundException($"User with Id: {id} not found");
+        var user = await _dbContext.Users
+                       .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                       .FirstOrDefaultAsync(user => user.Id == id)
+                   ?? throw new NotFoundException($"User with Id: {id} not found");
+        
+        CheckPermissionAccess(user.Id);
+
+        return user;
     }
     
     public async Task<UserDto> GetByLoginAsync(UserLoginDto loginDto)
@@ -79,11 +91,13 @@ public class UserService : IUserService
         var user = await _dbContext.Users.FirstOrDefaultAsync(user => user.Id == id)
                 ?? throw new NotFoundException($"User with Id: {id} not found");
 
-        if (!CheckIfEmailIsAvailableAsync(userInputDto.Email).Result)
+        CheckPermissionAccess(user.Id);
+        
+        if (! await CheckIfEmailIsAvailableAsync(userInputDto.Email))
         {
             throw new BadRequestException("The user with this mail is already exists");
         }
-        
+
         userInputDto.Password = await HashPasswordAsync(user, userInputDto.Password);
         
         _mapper.Map(userInputDto, user);
@@ -100,10 +114,20 @@ public class UserService : IUserService
         var user = await _dbContext.Users.FirstOrDefaultAsync(user => user.Id == id)
                 ?? throw new NotFoundException($"User with Id: {id} not found");
         
+        CheckPermissionAccess(user.Id);
+        
         _dbContext.Users.Remove(user);
         await _dbContext.SaveChangesAsync();
     }
 
+    private void CheckPermissionAccess(int userId)
+    {
+        if (!(_currentUserInfo.Role == "Admin" || userId == _currentUserInfo.Id))
+        {
+            throw new BadRequestException("You do not have permission to access this user");
+        }
+    }
+    
     private async Task<bool> CheckIfEmailIsAvailableAsync(string email)
     {
         return await _dbContext.Users
@@ -133,10 +157,9 @@ public class UserService : IUserService
             await _dbContext.SaveChangesAsync();
         }
     }
+    
     private async Task<string> HashPasswordAsync(User user, string password)
     {
         return await Task.Run(() => _passwordHasher.HashPassword(user, password));
     }
-
-    
 }
